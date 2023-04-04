0b7ff529f6a822dd7f55c665bf666c8813ee63ac
Jason Daro <23290574+jasonbdaro@users.noreply.github.com>
2023-04-05 01:01:41 +0800

    added SsgwsgApi facade

--- src/Facades/SsgwsgApi.php
+++ src/Facades/SsgwsgApi.php
@@ -4,7 +4,7 @@ namespace Jasonbdaro\Ssgwsg\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
-class Ssgwsg extends Facade
+class SsgwsgApi extends Facade
 {
     /**
      * Get the registered name of the component.

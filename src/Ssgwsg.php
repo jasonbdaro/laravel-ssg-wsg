3485f69ec40420c2ea5f69f5c059646c9a21d93e
Jason Daro <23290574+jasonbdaro@users.noreply.github.com>
2023-05-05 01:45:58 +0800

    added attendance to facade

--- src/Ssgwsg.php
+++ src/Ssgwsg.php
@@ -5,12 +5,18 @@ namespace Jasonbdaro\Ssgwsg;
 use Jasonbdaro\Ssgwsg\Resources\CourseRun;
 use Jasonbdaro\Ssgwsg\Resources\Enrolment;
 use Jasonbdaro\Ssgwsg\Resources\Assessment;
+use Jasonbdaro\Ssgwsg\Resources\Attendance;
 
 class Ssgwsg
 {
     public function courseRun()
     {
         return new CourseRun;
+    }
+    
+    public function attendance()
+    {
+        return new Attendance;
     }
 
     public function enrolment()
